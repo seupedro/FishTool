3d83457e89b720ea09504a0cb2048070235a4e92
nakamoto <hartmnn.p@gmail.com>
2018-05-03 07:49:13 -0300

    minor changes

--- app/src/main/java/com/example/nakamoto/fishtool/AquaInfo.java
+++ app/src/main/java/com/example/nakamoto/fishtool/AquaInfo.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AquaMain extends AppCompatActivity {
+public class AquaInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aqua_main);
+        setContentView(R.layout.activity_aqua_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/example/nakamoto/fishtool/MainActivity.java
+++ app/src/main/java/com/example/nakamoto/fishtool/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TextView textView = findViewById(R.id.text1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AquaInfo.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
