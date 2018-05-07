89363e3f2f8e963d26d21b67ce3d80920e87ea6e
nakamoto <hartmnn.p@gmail.com>
2018-05-07 12:20:31 -0300

    Create and Read DB implemented

---
+++ app/src/main/java/com/example/nakamoto/fishtool/database/AquaContract.java
@@ -0,0 +1,55 @@
+package com.example.nakamoto.fishtool.database;
+
+import android.provider.BaseColumns;
+
+public class AquaContract {
+
+    private AquaContract(){}
+
+    public static class AquaEntry implements BaseColumns{
+
+        /* Table Constant */
+        public static final String AQUA_TABLE = "aquarium";
+
+        /* Columns Constants */
+        public static final String _aquaID = BaseColumns._ID;
+        public static final String NAME_COLUMN = "name";
+        public static final String DATE_AQUA_COLUMN = "date";
+        public static final String TYPE_COLUMN = "type";
+        public static final String STATUS_COLUMN = "status";
+        public static final String CO2_COLUMN = "co2";
+        public static final String DOSAGE_COLUMN = "dosage";
+        public static final String SUBSTRATE_COLUMN = "substrate";
+        public static final String NOTES_COLUMN = "notes";
+    }
+
+    public static class ParamEntry implements BaseColumns{
+
+        /* Table Constant */
+        public static final String PARAM_TABLE = "parameters";
+
+        /* Columns Constants */
+        // TODO: insert all tables
+        public static final String _paramID = BaseColumns._ID;
+        public static final String PH_COLUMN = "ph";
+        public static final String NH3_COLUMN = "nh3";
+        public static final String DATE_PARAM_COLUMN = "date";
+        public static final String AQUA_FKEY = "aqua_fkey";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
