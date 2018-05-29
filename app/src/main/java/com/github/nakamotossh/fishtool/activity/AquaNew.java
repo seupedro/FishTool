9a9e6750607264f505842110e1f8cd1a117ee5c2
nakamoto <hartmnn.p@gmail.com>
2018-05-29 11:09:33 -0300

    big changes

--- app/src/main/java/com/github/nakamotossh/fishtool/activity/AquaNew.java
+++ app/src/main/java/com/github/nakamotossh/fishtool/activity/AquaNew.java
@@ -1,57 +1,15 @@
 package com.github.nakamotossh.fishtool.activity;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.content.ContentValues;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.github.nakamotossh.fishtool.R;
-import com.github.nakamotossh.fishtool.database.AquaDbHelper;
-import com.myhexaville.smartimagepicker.ImagePicker;
-import com.myhexaville.smartimagepicker.OnImagePickedListener;
+import com.github.nakamotossh.fishtool.fragments.NewFragment;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.AQUA_CONTENT_URI;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.AQUA_TABLE;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.CO2_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.DATE_AQUA_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.DOSAGE_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.FILTER_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.IMAGE_URI_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.LIGHT_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.LITERS_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.NAME_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.NOTES_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.SIZE_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.STATUS_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.SUBSTRATE_COLUMN;
-import static com.github.nakamotossh.fishtool.database.AquaContract.AquaEntry.TYPE_COLUMN;
 import static com.github.nakamotossh.fishtool.debug.WakeUp.riseAndShine;
+import static com.github.nakamotossh.fishtool.fragments.NewFragment.onChanged;
 
 public class AquaNew extends AppCompatActivity {
 
@@ -59,34 +17,6 @@ public class AquaNew extends AppCompatActivity {
     //TODO: [OPTIMIZE] Realease resource: Close Cursor, Close Db
 
     private static final String TAG = "NEWAQUA";
-
-    private ImageView aquaImage;
-    private EditText aquaName;
-    private static EditText aquaDate;
-    private Spinner aquaType;
-    private Spinner aquaStatus;
-    private EditText aquaLiters;
-    private EditText aquaLight;
-    private EditText aquaCo2;
-    private EditText aquaDose;
-    private EditText aquaSubstrate;
-    private EditText aquaNote;
-    private EditText aquaSize;
-    private EditText aquaFilter;
-    private FloatingActionButton fab;
-    private AquaDbHelper dbHelper;
-    private ImageButton getDateButton;
-    private ImagePicker imagePicker;
-    private Uri photoUri = null;
-
-    private boolean onChanged = false;
-    private View.OnTouchListener touchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            onChanged = true;
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,122 +26,13 @@ public class AquaNew extends AppCompatActivity {
         /* TODO: remove before release */
         riseAndShine(this);
 
-        /* Find views on layout */
-        aquaImage = findViewById(R.id.aqua_photo);
-        aquaName = findViewById(R.id.aqua_name);
-        aquaSize = findViewById(R.id.aqua_size);
-        aquaType = findViewById(R.id.aqua_type);
-        aquaStatus = findViewById(R.id.aqua_status);
-        aquaLiters = findViewById(R.id.aqua_liters);
-        aquaLight = findViewById(R.id.aqua_light);
-        aquaCo2 = findViewById(R.id.aqua_co2);
-        aquaSubstrate = findViewById(R.id.aqua_substrate);
-        aquaDose = findViewById(R.id.aqua_dose);
-        aquaDate = findViewById(R.id.aqua_date);
-        aquaNote = findViewById(R.id.aqua_note);
-        aquaFilter = findViewById(R.id.aqua_filter);
-        fab = findViewById(R.id.fab);
-        getDateButton = findViewById(R.id.date_button);
-
-        /* Track changes */
-        getDateButton.setOnTouchListener(touchListener);
-        fab.setOnTouchListener(touchListener);
-        aquaName.setOnTouchListener(touchListener);
-        aquaType.setOnTouchListener(touchListener);
-        aquaStatus.setOnTouchListener(touchListener);
-        aquaLiters.setOnTouchListener(touchListener);
-        aquaLight.setOnTouchListener(touchListener);
-        aquaCo2.setOnTouchListener(touchListener);
-        aquaSubstrate.setOnTouchListener(touchListener);
-        aquaDose.setOnTouchListener(touchListener);
-        aquaDate.setOnTouchListener(touchListener);
-        getDateButton.setOnTouchListener(touchListener);
-        aquaNote.setOnTouchListener(touchListener);
-        aquaSize.setOnTouchListener(touchListener);
-
-        /* Open DB Connection */
-        dbHelper = new AquaDbHelper(this);
-
-        /* Set listener on fab */
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* Image Picker! */
-                imagePicker = new ImagePicker(AquaNew.this, null, new OnImagePickedListener() {
-                    @Override
-                    public void onImagePicked(Uri imageUri) {
-                        aquaImage.setImageURI(imageUri);
-                        photoUri = imageUri;
-                    }
-                }).setWithImageCrop(1,1);
-
-                /* Ask where get images from */
-                AlertDialog.Builder builder = new AlertDialog.Builder(AquaNew.this);
-                builder.setTitle("Add Photo");
-                /* Dialog Option */
-                final int CAMERA_OPTION = 0;
-                final int GALLERY_OPTION = 1;
-                builder.setItems(new CharSequence[]{"Camera", "Gallery"},
-                        new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int whichOption) {
-                        if (whichOption == CAMERA_OPTION){
-                            imagePicker.openCamera();
-                        }
-                        if (whichOption == GALLERY_OPTION){
-                            imagePicker.choosePicture(false);
-                        }
-                    }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (dialog !=  null){
-                            dialog.dismiss();
-                        }
-                    }
-                });
-                builder.create().show();
-            }
-        });
-
-        /* Show Date Picker on date field */
-        aquaDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePickerDialog(v);
-            }
-        });
-
-        /* Get current date on Click */
-        getDateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                String currentDate = simpleDateFormat.format(calendar.getTime());
-                aquaDate.setText(currentDate);
-            }
-        });
+        /* Start Fragment */
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.frag_conteiner, new NewFragment())
+                .commit();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        imagePicker.handleActivityResult(resultCode, requestCode, data);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        imagePicker.handlePermission(requestCode, grantResults);
-    }
-
-    @Override
-    protected void onDestroy() {
-        dbHelper.close();
-        super.onDestroy();
-    }
 
     @Override
     public void onBackPressed() {
@@ -238,153 +59,6 @@ public class AquaNew extends AppCompatActivity {
         }
     }
 
-    private void readFromDb() {
-        /* Get database */
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-        /* Define data retrive */
-        Cursor c = db.query(
-                AQUA_TABLE,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null
-        );
 
-        c.moveToFirst();
-        aquaImage.setImageURI(Uri.parse(c.getString(c.getColumnIndexOrThrow(IMAGE_URI_COLUMN))));
-        aquaName.setText(c.getString(c.getColumnIndexOrThrow(NAME_COLUMN)));
-        aquaDate.setText(c.getString(c.getColumnIndexOrThrow(DATE_AQUA_COLUMN)));
-        aquaSize.setText(c.getString(c.getColumnIndexOrThrow(SIZE_COLUMN)));
-        aquaType.setSelection(c.getInt(c.getColumnIndexOrThrow(TYPE_COLUMN)));
-        aquaStatus.setSelection(c.getInt(c.getColumnIndexOrThrow(STATUS_COLUMN)), true);
-        aquaLiters.setText(c.getString(c.getColumnIndexOrThrow(LITERS_COLUMN)));
-        aquaFilter.setText(c.getString(c.getColumnIndexOrThrow(FILTER_COLUMN)));
-        aquaLight.setText(c.getString(c.getColumnIndexOrThrow(LIGHT_COLUMN)));
-        aquaCo2.setText(c.getString(c.getColumnIndexOrThrow(CO2_COLUMN)));
-        aquaDose.setText(c.getString(c.getColumnIndexOrThrow(DOSAGE_COLUMN)));
-        aquaSubstrate.setText(c.getString(c.getColumnIndexOrThrow(SUBSTRATE_COLUMN)));
-        aquaNote.setText(c.getString(c.getColumnIndexOrThrow(NOTES_COLUMN)));
-    }
 
-    private void saveValues() {
-
-        /* Parsing Fields to string */
-        String strAquaName = aquaName.getText().toString().trim();
-        String strAquaDate = aquaDate.getText().toString().trim();
-        String strAquaSize = aquaSize.getText().toString().trim();
-        String strAquaLiters = aquaLiters.getText().toString().trim();
-        String strAquaLight = aquaLight.getText().toString().trim();
-        String strAquaCo2 = aquaCo2.getText().toString().trim();
-        String strAquaFilter = aquaFilter.getText().toString().trim();
-        String strAquaDose = aquaDose.getText().toString().trim();
-        String strAquaSubstrate = aquaSubstrate.getText().toString().trim();
-        String strAquaNote = aquaNote.getText().toString().trim();
-        int intAquaStatus = ((int) aquaStatus.getSelectedItemId());
-        int intAquaType = ((int) aquaType.getSelectedItemId());
-
-        /* Get Database */
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        /* Insert data */
-        ContentValues values = new ContentValues();
-
-        /* Insert only filled fields */
-        if (photoUri != null){
-            values.put(IMAGE_URI_COLUMN, photoUri.toString());
-        }
-        /* Prevent saving without a name */
-        if (!TextUtils.isEmpty(strAquaName)){
-            values.put(NAME_COLUMN, strAquaName);
-        } else {
-            /* Give a feedback to the user */
-            Toast.makeText(this, "Name field cannot be empty", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (!TextUtils.isEmpty(strAquaDate)){
-            values.put(DATE_AQUA_COLUMN, strAquaDate);
-        }
-        if (!TextUtils.isEmpty(strAquaSize)){
-            values.put(SIZE_COLUMN, strAquaSize);
-        }
-        if (!TextUtils.isEmpty(strAquaLiters)){
-            values.put(LITERS_COLUMN, strAquaLiters);
-        }
-        if (!TextUtils.isEmpty(strAquaFilter)){
-            values.put(FILTER_COLUMN, strAquaFilter);
-        }
-        if (!TextUtils.isEmpty(strAquaLight)){
-            values.put(LIGHT_COLUMN, strAquaLight);
-        }
-        if (!TextUtils.isEmpty(strAquaCo2)){
-            values.put(CO2_COLUMN, strAquaCo2);
-        }
-        if (!TextUtils.isEmpty(strAquaDose)){
-            values.put(DOSAGE_COLUMN, strAquaDose);
-        }
-        if (!TextUtils.isEmpty(strAquaSubstrate)){
-            values.put(SUBSTRATE_COLUMN, strAquaSubstrate);
-        }
-        if (!TextUtils.isEmpty(strAquaNote)){
-            values.put(NOTES_COLUMN, strAquaNote);
-        }
-        /* Unnecessary verification */
-        values.put(TYPE_COLUMN, intAquaType);
-        values.put(STATUS_COLUMN, intAquaStatus);
-        /* Insert into Db */
-        Log.d(TAG, "saveValues: type = " + intAquaType +
-                ", status = " + intAquaStatus);
-        Uri uri = getContentResolver().insert(AQUA_CONTENT_URI, values);
-
-        /* TODO: [Remove] Debug purpose */
-        Toast.makeText(this, String.valueOf(uri), Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.new_aqua, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.save:
-                saveValues();
-                //finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-        Log.d(TAG, "showDatePickerDialog: ");
-    }
-
-    public static class DatePickerFragment extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-            Log.d(TAG, "onCreateDialog: ");
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            aquaDate.setText(day + "/" + month + "/" + year);
-            Log.d(TAG, "onDateSet: ");
-        }
-    }
 }
