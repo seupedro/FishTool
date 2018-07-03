f14eedc1052b9083f312e5b0b348194ce9ae4776
nakamoto <hartmnn.p@gmail.com>
2018-07-03 00:56:46 -0300

    Aqua Layout changes

--- app/src/main/java/com/github/nakamotossh/fishtool/adapters/AquaListCursorAdapter.java
+++ app/src/main/java/com/github/nakamotossh/fishtool/adapters/AquaListCursorAdapter.java
@@ -1,6 +1,5 @@
 package com.github.nakamotossh.fishtool.adapters;
 
-import android.animation.LayoutTransition;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -11,9 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.github.nakamotossh.fishtool.R;
@@ -29,8 +26,6 @@ public class AquaListCursorAdapter extends CursorRecyclerViewAdapter<AquaListCur
     //TODO: Fix Fab overrinding last aquarium
 
     private static final String TAG = "AQUA_ADAPTER";
-    private int mExpandedPosition = -1;
-    private int previousExpandedPosition;
     private Context mContext;
 
     public AquaListCursorAdapter(Context context, Cursor aquaCursor){
@@ -40,9 +35,7 @@ public class AquaListCursorAdapter extends CursorRecyclerViewAdapter<AquaListCur
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         CardView cardViewParent;
-        Button expandButton;
         ConstraintLayout constraintLayout;
-        TableLayout tableLayout;
         TextView title;
         TextView subtitle;
         ImageView photo;
@@ -50,9 +43,7 @@ public class AquaListCursorAdapter extends CursorRecyclerViewAdapter<AquaListCur
         public ViewHolder(View view) {
             super(view);
             cardViewParent = view.findViewById(R.id.card_parent);
-            expandButton = view.findViewById(R.id.expand_collapse_button);
             constraintLayout = view.findViewById(R.id.constraint_card_child);
-            tableLayout = view.findViewById(R.id.card_tableLayout);
             title = view.findViewById(R.id.title_card);
             subtitle = view.findViewById(R.id.subtitle_card);
             photo = view.findViewById(R.id.aqua_image);
@@ -62,7 +53,7 @@ public class AquaListCursorAdapter extends CursorRecyclerViewAdapter<AquaListCur
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.card_item, parent, false);
+                .inflate(R.layout.card_item_aqua, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
@@ -84,33 +75,6 @@ public class AquaListCursorAdapter extends CursorRecyclerViewAdapter<AquaListCur
         final String name = cursor.getString(cursor.getColumnIndexOrThrow(NAME_COLUMN));
         int type = cursor.getInt(cursor.getColumnIndexOrThrow(TYPE_COLUMN));
         String image = cursor.getString(cursor.getColumnIndexOrThrow(IMAGE_URI_COLUMN));
-
-        /* Set Layout Animations on Change */
-        viewHolder.cardViewParent.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-        viewHolder.constraintLayout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-
-        /* Check is expanded */
-        final int position = viewHolder.getAdapterPosition();
-        final boolean isExpanded = position == mExpandedPosition;
-
-        /* Display views on layout */
-        viewHolder.tableLayout.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
-        viewHolder.expandButton.setText(isExpanded ? "Collapse" : "Expand");
-        viewHolder.tableLayout.setActivated(isExpanded);
-
-        if (isExpanded) {
-            previousExpandedPosition = position;
-        }
-
-        viewHolder.expandButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mExpandedPosition = isExpanded ? -1 : position;
-                notifyItemChanged(previousExpandedPosition);
-                notifyItemChanged(position);
-            }
-        });
-
 
         /* Show aqua features */
         viewHolder.cardViewParent.setOnClickListener(new View.OnClickListener() {
